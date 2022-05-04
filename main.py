7f429eaec92d78c708f6414a514d083effa7d11a
Alex <bremov2003@mail.ru>
2022-05-04 15:26:15 +0500

    3 commit

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# #1 задание
-k = int(input("Введите левую границу : "))
-m = int(input("Введите правую границу : "))
-sum = 0
-for i in range(k, m + 1):
- sum += pow(i,2)
-print("Сумма квадратов = " + str(sum))
 # 2 задание
 import math
 print ("Введите натуральное число N")
